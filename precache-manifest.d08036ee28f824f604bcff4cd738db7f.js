4a95d7680564f166da4f8d3f94aa03a4cf0c84cc
Dmitry Chernushev <dmitry.chernyshev@omega-r.com>
2019-10-09 09:55:39 +0300

    Updates

---
+++ precache-manifest.d08036ee28f824f604bcff4cd738db7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797f0acd54fcc175e78e3c738831b3e2",
+    "url": "/figure/index.html"
+  },
+  {
+    "revision": "db1a96fa4e393a23fc2d",
+    "url": "/figure/static/css/main.61df1037.chunk.css"
+  },
+  {
+    "revision": "b2c9fa3c5123118da5fe",
+    "url": "/figure/static/js/2.a368402e.chunk.js"
+  },
+  {
+    "revision": "db1a96fa4e393a23fc2d",
+    "url": "/figure/static/js/main.d3476ade.chunk.js"
+  },
+  {
+    "revision": "fe26ed38f8196862ddd3",
+    "url": "/figure/static/js/runtime~main.35d6d341.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/figure/precache-manifest.fda6599d19a4f766767ff2bfc3457d8f.js"
+  "/figure/precache-manifest.d08036ee28f824f604bcff4cd738db7f.js"
 );
 
 self.addEventListener('message', (event) => {
